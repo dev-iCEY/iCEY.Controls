61334ef60ca22f6e216f62686082c7c3ff24f248
Yume <admin@himiko.pw>
2017-04-09 17:32:49 +0300

    Добавлены 2 стиля Slider

--- Yume.Controls/Yume.Controls/Properties/AssemblyInfo.cs
+++ Yume.Controls/Yume.Controls/Properties/AssemblyInfo.cs
@@ -1,24 +1,16 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
-[assembly: AssemblyTitle("Yume.Controls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Yume.Controls Library")]
+[assembly: AssemblyDescription("Yume.Controls Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yume Developer")]
 [assembly: AssemblyProduct("Yume.Controls")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Yume Developer 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
 //Чтобы начать создание локализуемых приложений, задайте
@@ -40,16 +32,5 @@ using System.Windows;
                                       // в приложении или в каких-либо словарях ресурсов для конкретной темы)
 )]
 
-
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//   Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
