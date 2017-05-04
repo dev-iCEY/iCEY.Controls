2f63e13ee88e2e0f75f30ea0fa20ffc09dafc2d1
Yume <admin@himiko.pw>
2017-05-04 19:43:57 +0300

    Rebuilding project files

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 @if exist "%ProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin" set PATH=%ProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%PATH%
 @if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin" set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%PATH%
 
-msbuild ".\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Debug;Platform=x86 /m
-msbuild ".\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Release;Platform=x86 /m
-msbuild ".\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Debug;Platform=x64 /m
-msbuild ".\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Release;Platform=x64 /m
+msbuild ".\src\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Debug;Platform=x86 /m
+msbuild ".\src\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Release;Platform=x86 /m
+msbuild ".\src\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Debug;Platform=x64 /m
+msbuild ".\src\Yume.Controls\Yume.Controls.sln" /t:Rebuild /p:Configuration=Release;Platform=x64 /m
